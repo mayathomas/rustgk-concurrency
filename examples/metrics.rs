6149ca58750409d1abacf622599391f6fb0f2ffd
maya <770257551@qq.com>
2024-07-18 11:19:10 +0800

    feature: rwlock for  metrics

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -10,7 +10,7 @@ const M: usize = 4;
 fn main() -> Result<()> {
     let metrics = Metrics::new();
 
-    println!("{:?}", metrics.snapshot());
+    println!("{}", metrics);
 
     for idx in 0..N {
         task_worker(idx, metrics.clone())?;
@@ -21,8 +21,8 @@ fn main() -> Result<()> {
     }
 
     loop {
-        thread::sleep(Duration::from_secs(5));
-        println!("{:?}", metrics.snapshot());
+        thread::sleep(Duration::from_secs(2));
+        println!("{}", metrics);
     }
 }
 
@@ -47,7 +47,7 @@ fn request_worker(metrics: Metrics) -> Result<()> {
             //do something...
             let mut rng = rand::thread_rng();
             thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
-            let page = rng.gen_range(1..256);
+            let page = rng.gen_range(1..5);
             metrics.inc(format!("req.page.{}", page))?;
         }
         //和metrics.inc(format!("req.page.{}", page))?;这一行的返回值要保持一致，都要是Result<(), anyhow::Error>
--- src/metrics.rs
+++ src/metrics.rs
@@ -1,23 +1,24 @@
 use anyhow::{anyhow, Result};
+use core::fmt;
 use std::{
     collections::HashMap,
-    sync::{Arc, Mutex},
+    sync::{Arc, RwLock},
 };
 
 #[derive(Debug, Clone)]
 pub struct Metrics {
-    data: Arc<Mutex<HashMap<String, i64>>>,
+    data: Arc<RwLock<HashMap<String, i64>>>,
 }
 
 impl Metrics {
     pub fn new() -> Self {
         Self {
-            data: Arc::new(Mutex::new(HashMap::new())),
+            data: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 
     pub fn inc(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self.data.lock().map_err(|e| anyhow!(e.to_string()))?;
+        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
         let counter = data.entry(key.into()).or_insert(0);
         *counter += 1;
         Ok(())
@@ -26,8 +27,18 @@ impl Metrics {
     pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
         Ok(self
             .data
-            .lock()
+            .read()
             .map_err(|e| anyhow!(e.to_string()))?
             .clone())
+    }
+}
+
+impl fmt::Display for Metrics {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let data = self.data.read().map_err(|_e| fmt::Error {})?;
+        for (key, value) in data.iter() {
+            writeln!(f, "{}: {}", key, value)?;
+        }
+        Ok(())
     }
 }
