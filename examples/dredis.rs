cfe911b8e2119711edecdc8bccbe3236696d163e
maya <770257551@qq.com>
2024-07-23 09:32:12 +0800

    feature: dummy redis

---
+++ examples/dredis.rs
@@ -0,0 +1,52 @@
+use std::{io, net::SocketAddr};
+
+use anyhow::Result;
+use tokio::{io::AsyncWriteExt, net::TcpListener};
+use tracing::{info, warn};
+
+const BUF_SIZE: usize = 4096;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+    let addr = "0.0.0.0:6379";
+    let listener = TcpListener::bind(addr).await?;
+    info!("Dredis: listening on:{}", addr);
+
+    loop {
+        let (stream, raddr) = listener.accept().await?;
+        info!("Accept connection from:{}", raddr);
+        tokio::spawn(async move {
+            if let Err(e) = process_redis_conn(stream, raddr).await {
+                warn!("Error processing conn with {}: {:?}", raddr, e);
+            }
+        });
+    }
+}
+
+async fn process_redis_conn(mut stream: tokio::net::TcpStream, raddr: SocketAddr) -> Result<()> {
+    loop {
+        stream.readable().await?;
+        let mut buf = Vec::with_capacity(BUF_SIZE);
+
+        // Try to read data, this may still fail with `WouldBlock`
+        // if the readiness event is a false positive.
+        match stream.try_read_buf(&mut buf) {
+            Ok(0) => break,
+            Ok(n) => {
+                info!("read {} bytes", n);
+                let line = String::from_utf8_lossy(&buf);
+                info!("{:?}", line);
+                stream.write(b"+Ok\r\n").await?;
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                continue;
+            }
+            Err(e) => {
+                return Err(e.into());
+            }
+        }
+    }
+    warn!("Connection {} closed", raddr);
+    Ok(())
+}
